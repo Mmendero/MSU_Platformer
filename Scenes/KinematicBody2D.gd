42d8b57da76de3ed7fbc1879a4af207ebe1a753e
Matthew Mendero <matthewmendero@gmail.com>
2019-11-19 22:19:15 -0500

    Finalize

--- Scenes/KinematicBody2D.gd
+++ Scenes/KinematicBody2D.gd
@@ -1,25 +1,39 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const SPEED = 200
+const MAX_SPEED = 200
+const ACCELERATION = 50
 const GRAVITY = 20
 const JUMP_HEIGHT = -500
 var motion = Vector2()
 
 func _physics_process(delta):
+	var friction = false
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x = 0
-		
+		$Sprite.play("Idle")
+		friction = true
+
 	if is_on_floor():
-		print("On floor.")
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
-	
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.05)
 	motion = move_and_slide(motion,UP)
 	pass
